05d0df7368b4e6e918abfc393262be41e55b7e3b
Brewster McCracken <mccrackenb@me.com>
2017-09-06 11:48:35 -0500

    updates to market reports

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // JS for Dataport site
 $('.dropdownContainer').hide();
 
+// sticky scroll
 $(window).on('scroll', function () {
   var distanceScrolled = $(window).scrollTop();
   console.log('The distance scrolled is: ' + distanceScrolled);
@@ -11,22 +12,13 @@ $(window).on('scroll', function () {
   }
 });
 
+// sidbar
 $('#menuButton').on('click', function() {
   $('main').addClass('bodyShift');
   $('.headerContent').fadeOut('350ms');
   $('.row2').addClass('headerShift');
   $('.menuOpenOpacity').show();
   $('nav').addClass('open');
-});
-
-$('.closeButton').on('click', function() {
-  if($('nav').hasClass('open')) {
-    $('nav').removeClass('open');
-    $('main').removeClass('bodyShift');
-    $('.row2').removeClass('headerShift');
-    $('.headerContent').fadeIn('350ms');
-    $('.menuOpenOpacity').fadeOut('350ms');
-  }
 });
 
 $('.menuOpenOpacity').on('click', function() {
@@ -39,6 +31,7 @@ $('.menuOpenOpacity').on('click', function() {
   }
 });
 
+// dropdown container
 $('.selectBox').on('click', function() {
   $('.dropdownContainer').slideToggle(300);
   $('.arrowTouchZone').toggleClass('turnArrow');
@@ -59,17 +52,15 @@ $('.row2').on('click', function() {
   $('.arrowTouchZone').removeClass('turnArrow');
   $('dropdown').removeClass('dropdownOpen');
 });
-$('div .orangeHeaderTab').on('click', function() {
-  $('div .orangeHeaderTab').removeClass('opacityTabActive');
-  $(this).addClass('opacityTabActive');
-});
 
+// refresh button
 var refreshAngle =0;
 $('#refreshButton').on('click', function() {
   refreshAngle += 360;
   $('.refreshTouchZone').css({'transform': 'rotate(' + refreshAngle + 'deg)'});
 });
 
+// checkbox
 $('.img-swap').on('click', function(){
     if ($(this).attr("class") == "img-swap") {
       this.src = this.src.replace("_off","_on");
@@ -80,4 +71,29 @@ $('.img-swap').on('click', function(){
     $('#liveContainer').slideToggle(350);
     $('#lastUpdated').toggle();
     $('div.pageBar').toggleClass('scrollBorder');
+});
+
+// subreport tabs to show
+$('div .orangeHeaderTab').on('click', function () {
+  $('div .orangeHeaderTab').removeClass('opacityTabActive');
+  $(this).addClass('opacityTabActive');
+  // $('.subreportDisplay').hide();
+  // var sectionToShow = $(this).attr('data-related-panel');
+  // $('#' + sectionToShow).show();
+});
+
+
+// subreport tab selection
+$('#sub-pricing1 .subreportTabBox').on('click', function() {
+  $('#sub-pricing1 .subreportTabBox').removeClass('tabBoxActive');
+  $(this).addClass('tabBoxActive');
+});
+
+// sandbochs report
+$('#liveReport2-1-1 a').on('click', function () {
+		$('#liveReport2-1-1 a').removeClass('orangeTabActive');
+		$(this).addClass('orangeTabActive');
+		$('.nav-panel').hide();
+		var sectionToShow = $(this).attr('data-related-panel');
+		$('#' + sectionToShow).show();
 });
